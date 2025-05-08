40b1cf37de46bf192a67485ac89bee184caa38ad
lydiaellingwood <lydiaellingwood@gmail.com>
2025-05-08 09:17:46 -0400

    added more to the player class, and started testing with multiple players

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from email.contentmanager import get_and_fixup_unknown_message_content
+
 from board import board
+from player import player
 
 
 def main():
     newBoard = board()
+    newBoard.PrintBoard()
+    p1 = player("Lydia","0")
+    p2 = player("Jack", "1")
+
+    newBoard.PlaceDisk(p1,3)
+    # for i in range(7):
+    #     newBoard.PlaceDisk(p2,3)
     newBoard.PrintBoard()
 
 main()
--- player.py
+++ player.py
@@ -1,6 +1,10 @@
 class player():
-    def __init__(self,name):
+    def __init__(self,name,symbol):
         self._name = name
+        self._symbol = symbol
 
-    def PlaceDisk(self):
+    def PlaceDisk(self,collumn):
         pass
+
+    def GetSymbol(self):
+        return self._symbol
